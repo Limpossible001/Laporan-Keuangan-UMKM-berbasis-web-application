f8fa07452f50b4cb4b249eb83a46187ce0257a27
Limpossible001 <260809931+Limpossible001@users.noreply.github.com>
2026-06-02 09:32:22 +0700

    aduiting cors 1.0

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        //boom
+        $middleware->validateCsrfTokens(except: [
+            'api/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
