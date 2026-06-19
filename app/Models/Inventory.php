4ce026eacec97752b8a5c37a4ad2f9ee6ca5eb55
Limpossible001 <260809931+Limpossible001@users.noreply.github.com>
2026-06-19 08:44:52 +0700

    left 1 more, done with migrations(1.2/2)

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Inventory extends Model
 {
@@ -14,4 +15,14 @@ class Inventory extends Model
     protected $casts = [
         'last_updated' => 'datetime',
     ];
+
+    public function purchases(): HasMany
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function sales(): HasMany
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
